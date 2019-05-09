2f7abef6e146ef54e34e622105b77c65dcc4b129
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-09 14:50:46 +0900

    print to UI

--- hello_world/main.js
+++ hello_world/main.js
@@ -3,11 +3,11 @@ const wasmObject = {env: {memory: memory}};
 
 WebAssembly.instantiateStreaming(fetch('main.wasm'), wasmObject)
     .then(obj => {
-        log(obj.instance.exports.helloWorld());
+        print(obj.instance.exports.helloWorld());
     });
 
-function log(length) {
+function print(length) {
     const bytes = new Uint8Array(memory.buffer, 0, length);
     const text = new TextDecoder('utf8').decode(bytes);
-    console.log(text);
+    document.getElementById("content").innerHTML = text;
 }
