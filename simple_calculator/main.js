a4074a8e6cc78cb5cb42abd96629ac9276ae3558
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-10 00:50:04 +0900

    move processes to wasm

--- simple_calculator/main.js
+++ simple_calculator/main.js
@@ -1,5 +1,3 @@
-let isAdd = true;
-let result = 0;
 let number = "";
 
 WebAssembly.instantiateStreaming(fetch('main.wasm'))
@@ -11,28 +9,17 @@ function onClickNumber(i) {
     number += i;
 }
 
-function calculate(exports) {
-    if (isAdd) {
-        result = exports.add(result, number);
-    } else {
-        result = exports.sub(result, number);
-    }
-    number = "";
-}
-
 function ready(obj) {
     const exports = obj.instance.exports;
     for (let i = 0; i < 10; i++) {
         document.getElementById(i).addEventListener("click", function() { onClickNumber(i); });
     }
     document.getElementById("sub").addEventListener("click", function() {
-        calculate(exports);
-        document.getElementById("result").innerHTML = result;
-        isAdd = false;
+        document.getElementById("result").innerHTML = exports.sub(parseInt(number));
+        number = "";
     });
     document.getElementById("add").addEventListener("click", function() {
-        calculate(exports);
-        document.getElementById("result").innerHTML = result;
-        isAdd = true;
+        document.getElementById("result").innerHTML = exports.add(parseInt(number));
+        number = "";
     });
 }
--- simple_calculator/main.wat
+++ simple_calculator/main.wat
@@ -1,9 +1,36 @@
 (module
-  (func (export "add") (param $left i32) (param $right i32) (result i32)
-        get_local $left
-        get_local $right
-        i32.add)
-  (func (export "sub") (param $left i32) (param $right i32) (result i32)
-        get_local $left
-        get_local $right
-        i32.sub))
+  (memory 1)
+  (func (export "add") (param i32) (result i32)
+        get_local 0
+        call $calculate
+        i32.const 0
+        i32.const 0
+        i32.store8
+        i32.const 1
+        i32.load)
+  (func (export "sub") (param i32) (result i32)
+        get_local 0
+        call $calculate
+        i32.const 0
+        i32.const 1
+        i32.store8
+        i32.const 1
+        i32.load)
+  (func $calculate (param i32)
+        i32.const 0
+        i32.load8_u
+        if
+            i32.const 1
+            i32.const 1
+            i32.load
+            get_local 0
+            i32.sub
+            i32.store
+        else
+            i32.const 1
+            i32.const 1
+            i32.load
+            get_local 0
+            i32.add
+            i32.store
+        end))
