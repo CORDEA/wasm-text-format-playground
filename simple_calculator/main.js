8b1e04daacf085c46783126402066ae0d40f1a92
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-10 13:02:21 +0900

    simplify

--- simple_calculator/main.js
+++ simple_calculator/main.js
@@ -1,18 +1,11 @@
 let number = "";
 
-WebAssembly.instantiateStreaming(fetch('main.wasm'))
-    .then(obj => {
-        ready(obj);
-    });
-
-function onClickNumber(i) {
-    number += i;
-}
+WebAssembly.instantiateStreaming(fetch('main.wasm')).then(obj => { ready(obj) });
 
 function ready(obj) {
     const exports = obj.instance.exports;
     for (let i = 0; i < 10; i++) {
-        document.getElementById(i).addEventListener("click", function() { onClickNumber(i); });
+        document.getElementById(i).addEventListener("click", function() { number += i });
     }
     document.getElementById("sub").addEventListener("click", function() {
         document.getElementById("result").innerHTML = exports.sub(parseInt(number));
